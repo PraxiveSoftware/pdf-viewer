d81dea80e2c6dee993bab674d1f9c569b3889f79
Jonas Franke <jonasfranke@sdevs.org>
2024-04-20 17:59:21 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# pdf-viewer
+> pdf viewer for praxive
+
+# Disclaimer
+originally created / published by [**@nandanprac**](https://github.com/nandanprac) on [**github**](https://github.com/nandanprac/electron-pdf-browser-window)
---
+++ test/index.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron')
+const PDFWindow = require('../')
+
+app.on('ready', () => {
+  const win = new PDFWindow({
+    width: 800,
+    height: 600
+  })
+
+  const win2 = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  const win3 = new BrowserWindow({
+    width: 100,
+    height: 100
+  })
+
+  win3.loadURL(`file://${__dirname}/remote.html`)
+
+  PDFWindow.addSupport(win2)
+
+  // win.loadURL('http://mozilla.github.io/pdf.js/web/compressed.tracemonkey-pldi-09.pdf')
+  // win.loadURL(`file://${__dirname}/helloworld.pdf`)
+  win.loadURL('https://arxiv.org/pdf/1606.05328.pdf?asdasd=a234')
+
+  // win2.loadURL('http://www.arxiv-sanity.com/')
+  win2.loadURL('https://arxiv.org/abs/1601.06759')
+})
